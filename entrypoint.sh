515e680a9f85534c68636b16e5330b6e61940421
Prans Daniel Simarmata <pransdaniel04@gmail.com>
2025-12-09 23:20:45 +0700

    update aqg ver 1.1.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
-exec gunicorn adaptive_engine.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 3
+exec gunicorn adaptive_engine.wsgi:application --bind 0.0.0.0:8000 --workers 3
